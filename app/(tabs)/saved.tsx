9dc2ca0029d0b90e6efaa9c2714187b18eae604c
Skybby15 <savaclaudiu42@gmail.com>
2025-09-20 16:23:21 +0300

    Home + Search + MovieDetails added

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,27 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+const Saved = () => {
+    return(
+        <View
+            className="bg-black flex-1 px-10"
+        >
+            <View
+                className="flex justify-center items-center flex-1 flex-col gap-5"
+            >
+                <Image source={icons.save}
+                    className="size-10"
+                    tintColor="#fff"
+                />
+                <Text
+                    className="text-gray-500"
+                >
+                    Profile
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+export default Saved
---
+++ services/appwrite.ts
@@ -0,0 +1,64 @@
+// track the searches made by user
+import { Client, ID, Query, TablesDB } from 'react-native-appwrite';
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const TABLE_NAME = process.env.EXPO_PUBLIC_APPWRITE_TABLE_NAME!;
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+
+const table = new TablesDB(client);
+
+export const updateSearchCount = async (query:string, movie: Movie) => {
+    try{
+        const result = await table.listRows({databaseId:DATABASE_ID,tableId:TABLE_NAME,
+            queries:[Query.equal('searchTerm',query)]
+        })
+        
+        if(result.rows.length > 0){
+            const existingMovie = result.rows[0];
+
+            await table.updateRow({
+                databaseId:DATABASE_ID,
+                tableId:TABLE_NAME,
+                rowId:existingMovie.$id,
+                data:{
+                    count: existingMovie.count + 1
+                }
+            })
+        } else {
+            await table.createRow({
+                databaseId:DATABASE_ID,
+                tableId:TABLE_NAME,
+                rowId: ID.unique(),
+                data:{
+                    searchTerm: query,
+                    movie_id: movie.id,
+                    title:movie.title,
+                    count: 1,
+                    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+    try {
+        const result = await table.listRows({databaseId:DATABASE_ID,tableId:TABLE_NAME,
+            queries:[
+                Query.limit(5),
+                Query.orderDesc('count'),
+            ]
+        })
+
+        return result.rows as unknown as TrendingMovie[];
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
+}
